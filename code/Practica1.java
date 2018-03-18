ff1ff7a6ab70ee0e2b5ab601997aeafbb0b2bcb0
Leonardo Daza <37496762+LeoDaza7@users.noreply.github.com>
2018-03-18 17:34:20 -0400

    Practica1
    se implementaron los cambios requeridos, sin embargo, solo funciona correcatmente con gamma entre [0,1[, reales y positivos

--- code/Practica1.java
+++ code/Practica1.java
@@ -1,94 +1,163 @@
-import java.util.ArrayList;
-import java.util.EnumMap;
-import java.util.Map;
+import java.util.*;
 
 public class Practica1 {
 
     enum State {
-        S1, S2, S3, S4;
-    }
-    enum Action {
-        a1, a2, a3, a4;
+
+        S1, S2, S3, S4
+
     }
 
-    private static Map<State, Map<Action, Integer> > rewards;
-    private static Map<State, Map<Action, State> > transitions;
+    enum Action {
+
+        a1, a2, a3, a4
+
+    }
+
+    static int [] [] rewardsMatrix = {{-1,1,1,2},
+                                    {-2,2,2,-2},
+                                    {0,-3,-2,3},
+                                    {2,-1,4,5}};
+
+    static State [] [] transitionsMatrix = {{State.S2,State.S4,State.S1,State.S1},
+                                            {State.S1,State.S3,State.S4,State.S2},
+                                            {State.S2,State.S1,State.S4,State.S2},
+                                            {State.S2,State.S4,State.S1,State.S4}};
+
+    private static Map<State, Map<Action, Integer>> rewards;
+    private static Map<State, Map<Action, State>> transitions;
     private static Map<State, Double> V;
     private static ArrayList<Double> Q;
+    private static Map<State, Boolean> C;
 
     private static void initTables( ) {
+
         rewards = new EnumMap<>( State.class );
         transitions = new EnumMap<>( State.class );
         V = new EnumMap<>( State.class );
+        C = new EnumMap<>(State.class);
+        int i = 0, j = 0;
+
         for ( State s: State.values() ) {
-            V.put( s, 0.0 );
+
+            V.put(s, 0.0);
+            C.put(s,false);
             rewards.put( s, new EnumMap<>( Action.class ) );
             transitions.put( s, new EnumMap<>( Action.class ) );
-        }
-        rewards.get( State.S1 ).put( Action.a1, -1 );
-        rewards.get( State.S1 ).put( Action.a2, -2 );
-        rewards.get( State.S1 ).put( Action.a3,  0 );
-        rewards.get( State.S1 ).put( Action.a4,  2 );
-        rewards.get( State.S2 ).put( Action.a1,  1 );
-        rewards.get( State.S2 ).put( Action.a2,  2 );
-        rewards.get( State.S2 ).put( Action.a3, -3 );
-        rewards.get( State.S2 ).put( Action.a4, -1 );
-        rewards.get( State.S3 ).put( Action.a1,  1 );
-        rewards.get( State.S3 ).put( Action.a2,  2 );
-        rewards.get( State.S3 ).put( Action.a3, -2 );
-        rewards.get( State.S3 ).put( Action.a4,  4 );
-        rewards.get( State.S4 ).put( Action.a1,  2 );
-        rewards.get( State.S4 ).put( Action.a2, -2 );
-        rewards.get( State.S4 ).put( Action.a3,  3 );
-        rewards.get( State.S4 ).put( Action.a4,  5 );
 
-        transitions.get( State.S1 ).put( Action.a1, State.S2 );
-        transitions.get( State.S1 ).put( Action.a2, State.S1 );
-        transitions.get( State.S1 ).put( Action.a3, State.S2 );
-        transitions.get( State.S1 ).put( Action.a4, State.S2 );
-        transitions.get( State.S2 ).put( Action.a1, State.S4 );
-        transitions.get( State.S2 ).put( Action.a2, State.S3 );
-        transitions.get( State.S2 ).put( Action.a3, State.S1 );
-        transitions.get( State.S2 ).put( Action.a4, State.S4 );
-        transitions.get( State.S3 ).put( Action.a1, State.S1 );
-        transitions.get( State.S3 ).put( Action.a2, State.S4 );
-        transitions.get( State.S3 ).put( Action.a3, State.S4 );
-        transitions.get( State.S3 ).put( Action.a4, State.S1 );
-        transitions.get( State.S4 ).put( Action.a1, State.S1 );
-        transitions.get( State.S4 ).put( Action.a2, State.S2 );
-        transitions.get( State.S4 ).put( Action.a3, State.S2 );
-        transitions.get( State.S4 ).put( Action.a4, State.S4 );
+        }
+
+        for (Action a : Action.values()){
+
+            for (State s : State.values()) {
+
+                rewards.get(s).put(a,rewardsMatrix[i][j]);
+                transitions.get(s).put(a,transitionsMatrix[i][j]);
+                j++;
+
+            }
+
+            i++;
+            j = 0;
+
+        }
+
+    }
+
+    private static void learn(double gamma) {
+
+        Q = new ArrayList<>();
+        double newV, lastV, epsilon=0.001;
+        int count = 1;
+
+        while (verify_C()) {
+
+            System.out.println("iteration: " + count);
+
+            for (State s : State.values()) {
+
+                lastV = eval_V(s);
+
+                for (Action a : Action.values()) {
+
+                    Q.add(eval_R(s, a) + gamma * eval_V(eval_T(s, a)));
+
+                }
+
+                V.put(s, Collections.max(Q));
+                newV = eval_V(s);
+
+                if (Math.abs(newV-lastV)<epsilon){
+
+                    C.put(s,true);
+
+                }
+
+                print_V();
+                Q.clear();
+
+            }
+
+            count++;
+
+        }
+
     }
 
     private static Integer eval_R( State s, Action a ) {
-	    return rewards.get( s ).get( a );
+
+        return rewards.get(s).get(a);
+
     }
 
     private static State eval_T( State s, Action a ) {
-	    return transitions.get( s ).get( a );
+
+        return transitions.get(s).get(a);
+
+    }
+
+    private static Double eval_V(State s){
+
+        return V.get(s);
+
+    }
+
+    private static Boolean verify_C(){
+
+        boolean b = true;
+
+        for(State s: State.values()){
+
+            b &= C.get(s);
+
+        }
+
+        return !b;
+
     }
 
     private static void print_V() {
-	    for ( State s: State.values() ) {
-		    System.out.print( V.get( s ) );
-		    System.out.print( ", " );
-	    }
-	    System.out.println();
+
+        for (State s: State.values()) {
+
+            System.out.print(V.get(s));
+            System.out.print(" ");
+
+        }
+
+        System.out.println();
+
     }
 
-    public static void main( String[] args ) {
+    public static void main (String[] args) {
+
+        System.out.print("gamma value(double):");
+        Scanner scanner = new Scanner(System.in);
+        double gamma = scanner.nextDouble();
+        scanner.close();
         initTables();
-	// cambio minimo para considerar que la convergencia sigue en proceso
-	double epsilon = 0.001;
-
-	// TODO:
-	// leer gamma del teclado
-	// implementar el aprendizaje según el algoritmo del enunciado
-	// imprimir los valores de V luego de cada iteración sobre todos los estados
-	// recordar (de algun modo) los valores del vector V de la anterior iteración
-	// terminar el proceso cuando el cambio de los números es menor a epsilon
+        learn(gamma);
 
     }
-
 
 }
